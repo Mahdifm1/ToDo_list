38dc806d2ca12cbee38a8eb1c9b7f74dd6cd6837
Mahdi Lotfian <80mahde@gmail.com>
2023-04-23 02:38:39 +0430

    add display of list of tasks

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,3 +1,5 @@
+from .models import Todo
+
 from django.shortcuts import render
 from django.utils import timezone
 from django.views.decorators.csrf import csrf_exempt
@@ -6,13 +8,14 @@ from django.http import HttpResponseRedirect
 
 
 def home_page(request):
-    return render(request, 'main_page.html')
+    print(Todo.objects.all().values())
+    contex = Todo.objects.all().values().order_by("added_date")
+    return render(request, 'main_page.html', context={'items': contex})
 
 
 @csrf_exempt
 def add_todo(request):
-    time = timezone.now()
+    time = timezone.now().date()
     text = request.POST['content']
     Todo.objects.create(title=text, added_date=time)
-    # print(request.POST)
     return HttpResponseRedirect('/')
