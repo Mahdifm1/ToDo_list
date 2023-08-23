5e7ea8e1dedf60e8293973b040320ab99340a273
Mahdi Lotfian <80mahde@gmail.com>
2023-08-23 23:45:42 +0330

    add user to models

--- todoapp/models.py
+++ todoapp/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Todo(models.Model):
+    user = models.ForeignKey(User, models.CASCADE)
     title = models.CharField(max_length=255)
     added_date = models.DateField()
