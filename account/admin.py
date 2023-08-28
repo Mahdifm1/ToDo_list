20d5226f451304a2fd986f5796d17ec446df2134
Mahdi Lotfian <80mahde@gmail.com>
2023-08-28 22:59:48 +0330

    add todo app admin page

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.template.response import TemplateResponse
+from collections import OrderedDict
 
-# Register your models here.
+from todoapp.models import Todo
+from .models import User
+
+
+class TodoInline(admin.StackedInline):
+    model = Todo
+    extra = 0
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'date_joined')
+    fields = ('username', 'email', 'date_joined')
+    readonly_fields = ('date_joined',)
+    inlines = (TodoInline,)
+    sortable_by = ('username', 'date_joined')
+    list_filter = ('date_joined',)
+    search_fields = ('username', 'email')
